95aa6b5c1d6db3a969bf4a9c8eb93617f0035b4f
Revanth2105 <revanthraobompelli@gmail.com>
2025-01-09 12:33:20 +0530

    Fibonacci Series

--- Demo019.java
+++ Demo019.java
@@ -1,13 +1,23 @@
 package Core_Java;
 
 import java.util.*;
-
 class Demo019{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        int rounded = (((num+2)/5)*5);
-        System.out.println("Rounded to the neared multiple of 5: "+rounded);
+        System.out.print("Enter number of digits: ");
+        int n=sc.nextInt();
+        Demo020 d = new Demo020();
+        d.getFibonacciSeries(n);
+    }
+    void getFibonacciSeries(int n){
+        System.out.println("Fibonacci series: ");
+        int a=0;
+        int b=1;
+        for(int i=0;i<n;i++){
+            System.out.print(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+        }
     }
 }
