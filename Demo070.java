24e8a19d6565e921c79bd83d6a1c8a5b6f3516e1
unknown <you@example.com>
2025-02-06 12:16:43 +0530

    Display prime no. in an array

--- Demo070.java
+++ Demo070.java
@@ -3,8 +3,12 @@ package Core_Java;
 class Demo070{
     public static void main(String[] args) {
         int[] arr = {12,20,30,43,11,10};
-        int i;
+        
         System.out.println("Prime numbers in the array are: ");
+        prime(arr);
+    }
+    static void prime(int[] arr){
+        int i;
         for(int n:arr){
             if(n==0 || n==1){
                 continue;
